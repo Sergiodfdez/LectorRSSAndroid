83344f212cd3bc43adf0bdd14b3e2bee339c3e14
igzsergiofernandez <sergio.fernandez@intelygenz.com>
2015-01-21 12:50:32 +0100

    ListView Added

---
+++ app/src/main/java/com/example/sergio/lectorrss/Noticia.java
@@ -0,0 +1,54 @@
+package com.example.sergio.lectorrss;
+
+import java.util.Date;
+
+/**
+ * Created by sergio on 21/01/15.
+ */
+public class Noticia {
+    private String titulo;
+    private String contenido;
+    private String enlace;
+    private String imagen;
+    private Date fecha;
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
